df7585e0d55947bc35997f3590a808ae4def6019
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-05-18 17:07:27 +0200

    initial project

---
+++ src/test/java/bachelor/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bachelor.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
